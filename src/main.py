934cdeb45519d18a187ca1e4b1d76eada8bf3d9f
Tomoya_Murakami2334 <ton2.tomoya010423@gmail.com>
2024-02-16 23:05:11 +0900

    langchainのpythonによる実装

--- src/main.py
+++ src/main.py
@@ -1,34 +1,169 @@
-import sys
+import dotenv
+dotenv.load_dotenv()
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain.agents import AgentExecutor, create_openai_functions_agent
+from langchain.tools.retriever import create_retriever_tool
+from langchain.agents.format_scratchpad import format_to_openai_function_messages
+from langchain_openai import ChatOpenAI
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain_core.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate)
+from typing import List
+from langchain.output_parsers import (
+    OutputFixingParser,
+    PydanticOutputParser,
+)
+from langchain_core.pydantic_v1 import BaseModel, Field
 
-sys.path.append("../")
-
-import constants as C
-import openai
-from openai import OpenAI
-import pandas as pd
 import utils
-from sqlalchemy import create_engine, inspect
-from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+import pandas as pd
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-args = sys.argv
-recipe = args[1]
-ingredient = args[2]
+app = Flask(__name__)
+CORS(app)
 
-"""
-このファイルはクックパッドからテキストを取得してデータベースに保存するためのファイルです。
-"""
+class Response(BaseModel):
+    """ユーザーに返す情報のスキーマ"""
+    title: str = Field(description="あなたが作成したレシピのタイトル")
+    ingredients: List[str] = Field(description="レシピで使用した材料のリスト")
+    instructions: List[str] = Field(description="レシピを作るための手順のリスト")
+    
+parser = PydanticOutputParser(pydantic_object=Response)
+# output_parser = StructuredOutputParser.from_response_schemas(parser.get_response_schemas())
+# format_instructions = parser.get_format_instructions()
+# print(format_instructions)
 
-if __name__ == "__main__":
+response_schemas = [
+    ResponseSchema(name="料理名", description="あなたが作成したレシピのタイトル", type="str"),
+    ResponseSchema(
+        name="材料",
+        description="レシピで使用した材料のリスト",
+        type="List[str]",
+    ),
+    ResponseSchema(
+        name="手順",
+        description="レシピを作るための手順のリスト",
+        type="List[str]",
+    ),
+]
+output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+format_instructions = output_parser.get_format_instructions()
+# print(format_instructions)
+
+@app.route("/api/data/<ingredient>/<recipe>", methods=["GET"])
+def main(ingredient, recipe):
+    # URLの設定
     url1 = "https://cookpad.com/search/" + recipe
     url2 = "https://cookpad.com/search/" + ingredient
     # データの取得
     # cookpad からデータを取得
-    df = utils.scrape_text(url1, limit=2)
-    df.to_csv(f"../log/{recipe}.csv", index=False)
-    df = utils.scrape_text(url2, limit=2)
+    df1,recipe_urls1 = utils.scrape_text(url1, limit=1)
+    # df.to_csv(f"../log/{recipe}.csv", index=False)
+    df2,recipe_urls2 = utils.scrape_text(url2, limit=1)
+    df = pd.concat([df1, df2])
     df.to_csv(f"../log/{ingredient}.csv", index=False)
     
-    # assistant api を使って新しいレシピを生成
-    response = utils.create_recipe(recipe, ingredient)
+    # csvファイルの読み込み
+    loader = CSVLoader(file_path=f'../log/{ingredient}.csv', csv_args={
+        'delimiter': ',',
+        'quotechar': '"',
+        'fieldnames': ["title","ingredient","instruction"]
+    })
+
+    # csvファイルの内容を分割
+    data = loader.load()
+    documents = RecursiveCharacterTextSplitter(
+    chunk_size=100, chunk_overlap=20
+    ).split_documents(data)
+    # ベクトル化
+    vector = FAISS.from_documents(documents, OpenAIEmbeddings())
+    retriever = vector.as_retriever()
+    # ツールの作成
+    retriever_tool = create_retriever_tool(
+        retriever,
+        "recipe_retriever",
+        "Search for information about recipes",
+    )
+    # ツールの登録
+    tools = [retriever_tool]
+    # チャットモデルの作成
+    llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
+    llm_with_tools = llm.bind_functions([retriever_tool, Response])
+    # プロンプトの設定
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system",
+                "ツールを使って既存のレシピを参照し、今までにない新しいレシピを作成してください。必ず、材料{ingredient}を使って料理{recipe}の作り方を教えてください。\
+                以下のフォーマットに従って回答してください。{format_instructions} \
+                出力フォーマット以外の情報は不要です。",
+            ),
+            ("user",
+                "{ingredient}を使った誰も食べたことのない{recipe}のレシピを教えてください。"
+            ),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
     
+    agent = (
+        {
+            "ingredient": lambda x: x["ingredient"],
+            "recipe": lambda x: x["recipe"],
+            "format_instructions": lambda x: '''
+            料理名：~ 
+            材料：
+            1. ~
+            2. ~
+            3. ~
+            手順：
+            1. ~
+            2. ~
+            3. ~
+            ''',
+            # Format agent scratchpad from intermediate steps
+            "agent_scratchpad": lambda x: format_to_openai_function_messages(
+                x["intermediate_steps"]
+            ),
+        }
+        | prompt
+        | llm_with_tools
+        | utils.parse
+    )
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=[retriever_tool],
+        verbose=True,
+    )
+    response = agent_executor.invoke(
+        {"ingredient": ingredient, "recipe": recipe},
+        return_only_outputs=True
+    )
+    try:
+        data = {
+            "content": response["output"],
+            "urls": {
+                "recipes": recipe_urls1,
+                "ingredients": recipe_urls2
+            }
+        }
+    except:
+        data = {
+            "content": None,
+            "urls": {
+                "recipes": recipe_urls1,
+                "ingredients": recipe_urls2
+            }
+        }
     
+    return jsonify(data)
+    
+if __name__ == "__main__":
+    # main("かぼちゃ", "カレーライス")
+    app.run(debug=True)
--- src/utils/scrape_text.py
+++ src/utils/scrape_text.py
@@ -19,10 +19,12 @@ def scrape_text(url: str, limit: int = 2) -> pd.DataFrame:
     Returns:
         Dataframe: クックパッドのページから取得したテキストとその他の情報
 
-        カラム：
-            url: URL
-            title: タイトル
-            text: テキスト
+                カラム：
+                    url: URL
+                    title: タイトル
+                    text: テキスト
+        
+        List: レシピのURL
 
     Error:
         Exception: station_exits not found
@@ -49,28 +51,40 @@ def scrape_text(url: str, limit: int = 2) -> pd.DataFrame:
         # detect_pattern.py で、station_exits が見つからなかった場合を解決しているはず
         raise Exception("text not found " + url)
     
-    df = pd.DataFrame(columns=["title", "instruction"])
+    df = pd.DataFrame(columns=["title", "ingredients", "instruction"])
+    recipe_urls = []
     for div in div_list:
         title      = div.find("a", class_="recipe-title").get_text(strip=True)
         my_logger.info(f"タイトル: {title}")
         recipe_url = div.find("a", class_="recipe-title").get("href")
         recipe_url = "https://cookpad.com" + recipe_url
+        recipe_urls.append(recipe_url)
         
         res_instruction = requests.get(recipe_url)
         soup_instruction = BeautifulSoup(res_instruction.text, "html.parser")
         
         div_instruction_list = soup_instruction.find_all("ol", class_="steps_wrapper", limit=10)
         div_instruction_list = div_instruction_list[0].find_all("p", class_="step_text")
+        
+        div_ingredients_list = soup_instruction.find_all("div", id="ingredients_list")
+        div_ingredients_list = div_ingredients_list[0].find_all("div", class_="ingredient_row")
+        
+        ingredients_all = ""
         text_all = ""
+        
+        for div_tmp in div_ingredients_list:
+            text = div_tmp.get_text(strip=True)
+            ingredients_all = ingredients_all + text + "\n"
+        
         
         for div_tmp in div_instruction_list:
             text = div_tmp.get_text(strip=True)
             text_all = text_all + text + "\n"
             
-        df_tmp = pd.DataFrame([[title, text_all]], columns=["title", "instruction"])
+        df_tmp = pd.DataFrame([[title, ingredients_all, text_all]], columns=["title", "ingredients", "instruction"])
         df = pd.concat([df, df_tmp], axis=0)
     
-    return df
+    return df, recipe_urls
     
 
 ####################
